cd43867fab51723d5e4d662020cbbc8573a2b134
Jack Meng <jiekemeng@gmail.com>
2018-05-24 08:45:58 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
-    <a href="/">主页</a><br>
-    <a href="/help">帮助</a><br>
-    <a href="/about">关于</a><br>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
