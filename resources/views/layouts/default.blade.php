5b1bfe1bcad04df1c5903ab8389682512430abbc
Jack Meng <jiekemeng@gmail.com>
2018-05-31 06:00:07 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,6 +12,7 @@
 @include('layouts._header')
 <div class="container">
     <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
     </div>
